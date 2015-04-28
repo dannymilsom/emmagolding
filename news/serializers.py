18e99831a6a7c67d3f6e91a2d5b101a30fdc2a99
Danny Milsom <dannysmilsom@gmail.com>
2015-04-28 16:35:06 +0100

    Format the datetime object returned by the API serializer for articles

--- news/serializers.py
+++ news/serializers.py
@@ -1,9 +1,11 @@
 from news.models import Article
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, DateTimeField
 
 class ArticleSerializer(ModelSerializer):
     """Serializer options for Article Model"""
+
+    created = DateTimeField(format='%b %m')
 
     class Meta:
         model = Article
